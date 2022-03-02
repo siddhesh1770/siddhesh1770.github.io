4a7133d0671ce5a29de2027bfc64805a565aad51
Siddhesh Patil <sidpatil1770@gmail.com>
2022-03-02 14:19:08 +0530

    last bug :

--- payments/main.js
+++ payments/main.js
@@ -1,6 +1,15 @@
 function changeLink() {
     const amount = document.getElementById("amount").value;
-    const url = `upi://pay?pn=SIDDHESH%20RAMCHANDRA%20PATIL&pa=sidpatil1770@okhdfcbank&cu=INR&am=${amount}&tn=From%20Website`;
+    const pn = "pn=SIDDHESH%20RAMCHANDRA%20PATIL"; // Reciever name
+    const am = `am=${amount}`; // amount
+    const pa = "pa=paytmqr281005050101h17vvz9lx3o2@paytm" // upi address
+    const cu = "cu=INR"; // currency
+    const mc = "mc=5499"; // merchant code
+    const mode = "mode=02"; // mode
+    const orgid = "orgid=000000"; // orgid
+    const paytmqr = "paytmqr=281005050101H17VVZ9LX3O2"
+    const sign = "sign=MEQCIGQBQVpl0pgTPT8DxWdzWlBJLoEKGo+7XjGTa+qFDRRaAiAaOcO/R4lQueZ/dxFrRGC9G7TfNdv+gE3NXdnaoGAODA=="; // signature
+    const url = "upi://pay?" + pn + "&" + am + "&" + pa + "&" + cu + "&" + mc + "&" + mode + "&" + orgid + "&" + paytmqr + "&" + sign;
     const upi = document.getElementById("upi");
     console.log(url)
     console.log(amount)
@@ -18,3 +27,5 @@ function checkLink() {
 
 document.getElementById("amount").addEventListener("keyup", changeLink);
 document.getElementById("upi").addEventListener("click", checkLink);
+
+// upi://pay?pa=paytmqr281005050101h17vvz9lx3o2@paytm&pn=Paytm%20Merchant&mc=5499&mode=02&orgid=000000&paytmqr=281005050101H17VVZ9LX3O2&sign=MEQCIGQBQVpl0pgTPT8DxWdzWlBJLoEKGo+7XjGTa+qFDRRaAiAaOcO/R4lQueZ/dxFrRGC9G7TfNdv+gE3NXdnaoGAODA==
